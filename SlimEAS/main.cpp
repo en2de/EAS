e02fe55e79c8fc97833902f17e02f25f13011f65
envy.chen <envx.chen@gmail.com>
2014-12-14 20:37:11 +0800

    Initial Commit

---
+++ SlimEAS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SlimEAS
+//
+//  Created by envy.chen on 12/14/14.
+//  Copyright (c) 2014 Slim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
