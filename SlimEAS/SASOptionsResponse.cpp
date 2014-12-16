f99090b9d876acc1f53ec7315e0bbdc17f3e6656
envy.chen <envx.chen@gmail.com>
2014-12-16 22:30:11 +0800

    add SASOptionsResponse

---
+++ SlimEAS/SASOptionsResponse.cpp
@@ -0,0 +1,9 @@
+//
+//  SASOptionsResponse.cpp
+//  SlimEAS
+//
+//  Created by envy.chen on 12/16/14.
+//  Copyright (c) 2014 Slim. All rights reserved.
+//
+
+#include "SASOptionsResponse.h"
---
+++ SlimEAS/SASOptionsResponse.h
@@ -0,0 +1,14 @@
+//
+//  SASOptionsResponse.h
+//  SlimEAS
+//
+//  Created by envy.chen on 12/16/14.
+//  Copyright (c) 2014 Slim. All rights reserved.
+//
+
+#ifndef __SlimEAS__SASOptionsResponse__
+#define __SlimEAS__SASOptionsResponse__
+
+#include <stdio.h>
+
+#endif /* defined(__SlimEAS__SASOptionsResponse__) */
